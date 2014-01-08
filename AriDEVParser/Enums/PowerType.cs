ceb64d6264b15e03727c23485b550f0f88276795
AriDEV <aridev666@gmail.com>
2014-01-08 16:51:38 +0100

    Core/Enum: Updated PowerType Enum 5.4.2 17688

--- AriDEVParser/Enums/PowerType.cs
+++ AriDEVParser/Enums/PowerType.cs
@@ -7,8 +7,18 @@ namespace AriDEVParser.Enums
         Rage = 1,
         Focus = 2,
         Energy = 3,
-        Happiness = 4,
+        LightForce = 4,
         Rune = 5,
-        RunicPower = 6
+        RunicPower = 6,
+        SoulShard = 7,
+        Eclipse = 8,
+        HolyPower = 9,
+        AlternatePower = 10,
+        DarkForce = 11,
+        Chi = 12,
+        ShadowOrb = 13,
+        BurningEmber = 14,
+        DemonicFury = 15,
+        ArcaneCharge = 16
     }
 }
